24f32e727fd34489c59411ea4d3601eb4c873b40
root <emploi.neveu@gmail.com>
2018-11-23 10:56:36 +0100

    exo7

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+$département = array (
+    69 => 'Nord-Pas-Calais',
+    80 => 'Somme',
+    02 => 'Aisne',
+    59 => 'Nord',
+    51 => 'Marne',
+    60 => 'Oise');
+    ?>
+  </body>
+</html>
